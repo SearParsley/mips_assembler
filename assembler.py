060815607fe902b93349a5f7a2dbb03af411d5f6
SearParsley <searparsley@gmail.com>
2024-04-09 17:06:49 -0600

    worked on data processing

--- assembler.py
+++ assembler.py
@@ -1,31 +1,63 @@
 COMMENT_CHAR = '#'
 
+DEBUG = True
+
 
 # Preprocess the lines to remove comments and whitespace
-def preprocess_lines(lines:list):
+def preprocess_lines(lines_list:list):
 
   # remove comments and trailing whitespace
-  for i in range(len(lines)):
-    lines[i] = lines[i].split(COMMENT_CHAR, 1)[0].strip()
+  for i in range(len(lines_list)):
+    lines_list[i] = lines_list[i].split(COMMENT_CHAR, 1)[0].strip()
 
   # remove empty lines
-  lines = filter(lambda x: x != '', lines)
+  new_lines_list = list(filter(lambda x: x != '', lines_list))
 
-  return lines
+  if DEBUG: print(new_lines_list)
 
+  return new_lines_list
 
 
 
 # TODO: Use the preprocessed program to build data table
-def build_data_table(lines:list):
-  return lines, lines, lines
+def build_data_table(lines_list:list):
+  data_table = {}
+  data_list = []
+
+  # split instructions based on headers
+  text_header_index = 0
+  try: text_header_index = lines_list.index('.text')
+  except: pass
+
+  data_header_index = -1
+  try: data_header_index = lines_list.index('.data')
+  except: pass
+
+  # return if data section is empty
+  if data_header_index+1 == text_header_index:
+    return data_table, data_list, lines_list
+  
+  for i in range(data_header_index, text_header_index):
+    # TODO: map of labels to memory addresses (data_table)
+
+    # TODO: list of data values (data_list)
+    pass
+
+  # FIXME: remove headers before? or after? figure it out
+
+  new_lines_list = []
+
+  if DEBUG:
+    print(data_table)
+    print(data_list)
+  
+  return data_table, data_list, new_lines_list
 
 
 
 # TODO: Build a label table and strip out the labels from the code
 def create_label_table(lines:list):
   return lines, lines
-
 
 
 
@@ -43,8 +75,6 @@ def post_process(asdasd):
 
 
 
-
-
 def main():
   # Defining the assembly file to read from
   filename = "test1.asm"
@@ -54,21 +84,26 @@ def main():
     lines:list = infile.readlines()
 
   lines = preprocess_lines(lines)
-  print(lines)
 
   data_table, data_list, lines = build_data_table(lines)
 
-  label_table, lines = create_label_table(lines)
+  # label_table, lines = create_label_table(lines)
 
-  encoded_program = encode_program(lines, label_table, data_table)
+  # encoded_program = encode_program(lines, label_table, data_table)
 
-  # Convert the strings to hexadecimal and write them to a file
-  hex_program = post_process(encoded_program)
-  with open("output.hex", "w") as outfile:
-      outfile.write("v3.0 hex words addressed\n00: ")
-      outfile.writelines(hex_program)
+  # # Convert the strings to hexadecimal and write them to a file
+  # hex_program = post_process(encoded_program)
+  # with open("output.hex", "w") as outfile:
+  #     outfile.write("v3.0 hex words addressed\n00: ")
+  #     outfile.writelines(hex_program)
 
-  # Convert the data list to hexadecimal and write it to a file
-  with open("data.hex", "w") as outfile:
-      outfile.write("v3.0 hex words addressed\n00: ")
-      outfile.writelines([f"{d:04x} " for d in data_list])
+  # # Convert the data list to hexadecimal and write it to a file
+  # with open("data.hex", "w") as outfile:
+  #     outfile.write("v3.0 hex words addressed\n00: ")
+  #     outfile.writelines([f"{d:04x} " for d in data_list])
+
+
+
+
+
+main()
